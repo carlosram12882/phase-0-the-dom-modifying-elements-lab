567b50c1382c58dbf62d317665bac3846a10da52
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-28 11:09:49 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove("main");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = 'Carlos is the champion'
